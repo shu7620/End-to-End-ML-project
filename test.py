cc18b905fc2870d9b281f9c8690967ebdeaa6bb6
Shubham Mandal <shubhambqa@gmail.com>
2026-06-26 00:42:02 +0530

    Model Evaluation Pipeline completed

---
+++ test.py
@@ -0,0 +1,9 @@
+# MLFLOW URI->https://dagshub.com/shu7620/End-to-End-ML-project.mlflow
+
+# import dagshub
+# dagshub.init(repo_owner='shu7620', repo_name='End-to-End-ML-project', mlflow=True)
+
+# import mlflow
+# with mlflow.start_run():
+#   mlflow.log_param('parameter name', 'value')
+#   mlflow.log_metric('metric name', 1)
